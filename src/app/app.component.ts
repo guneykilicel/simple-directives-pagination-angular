86ab64e49b471a91097e0f29d5437489b89a15ec
guneykilicel <kilicelguney@gmail.com>
2023-07-04 16:49:09 +0300

    onceki ve sonraki ve disabled durumlari

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentPage=1;
+  currentPage=0;
   images= [
     {
       title: 'Deniz Kum Güneş',
