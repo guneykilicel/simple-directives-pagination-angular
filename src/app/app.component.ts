e30859d285314c11a96f8fabf472b7ff0b5ace12
guneykilicel <kilicelguney@gmail.com>
2023-07-04 17:13:21 +0300

    sayfa degistrimeyi hem fonksiyon hem direkt yaptık

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,8 @@ export class AppComponent {
       url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60'
     }
   ]
+
+  changePage(number:number) {
+    this.currentPage = number;
+  }
 }
